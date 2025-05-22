9cdd3bbe6bfd0904cd588d6cc48eb7a930e55670
Aaron Gulman <enggulman@gmail.com>
2025-05-22 09:50:41 -0400

    refactor: switch from string-based color parsing to structured ARGB Lua tables

--- src/ui_bridge.rs
+++ src/ui_bridge.rs
@@ -5,29 +5,38 @@ pub struct UIStore{
     pub bg: Brush,
     pub t: Brush,
 }
-fn color_parser(element: String) -> Color{
-    let element:Color = Color::from_rgb_f32(element.parse::<f32>().unwrap(),
-                                            element.parse::<f32>().unwrap(),
-                                            element.parse::<f32>().unwrap());
-    element
+pub fn from_argb_f32(a:f32,r:f32,g:f32,b:f32) -> Color{
+    let rgb_color = Color::from_argb_f32(a,r,g,b);
+    rgb_color
 }
-
-
 
 pub fn ui_bridge() -> UIStore{
     let lua = Lua::new();
     lua.load(include_str!("../lua_scripts/ui_config.lua")).exec().unwrap();
     let globals = lua.globals();
     let config: Table = globals.get("config").unwrap();
-    let bg: String = config.get("background").unwrap();
-    let txt: String = config.get("text_color").unwrap();
-
-    let bg_color:Color = color_parser(bg);
+    
+    let bg_table: Table = config.get("background").unwrap();
+    let a:f32 = bg_table.get(1).unwrap();
+    let r:f32 = bg_table.get(2).unwrap();
+    let g:f32 = bg_table.get(3).unwrap();
+    let b:f32 = bg_table.get(4).unwrap();
+    
+    let bg_color = from_argb_f32(a/255.0,r/255.0,g/255.0,b/255.0);
     let bg_brush = Brush::from(bg_color);
 
-    let t_color:Color = color_parser(txt);
+    let txt_table: Table = config.get("text_color").unwrap();
+    let t_a:f32 = txt_table.get(1).unwrap();
+    let t_red:f32 = txt_table.get(2).unwrap();
+    let t_green:f32 = txt_table.get(3).unwrap();
+    let t_blue:f32 = txt_table.get(4).unwrap();
+    
+    println!("{} {} {}",r,g,b);
 
-    let t_brush = Brush::from(t_color);
+    let t_color = from_argb_f32(t_a/255.0,t_red/255.0,t_green/255.0,t_blue/255.0);
+    let t_brush  = Brush::from(t_color);
+
+    println!("Background color: {}",bg_color);
 
     UIStore{bg: bg_brush,t: t_brush}
 
